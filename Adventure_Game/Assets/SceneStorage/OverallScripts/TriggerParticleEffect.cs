c2369df88e5b3e52f7d9e13e25729275d78f81b9
blahaj <bn@bnchomes.com>
2025-04-11 23:45:28 -0600

    Scripting 12 Lab work
    built a Pathfinding script and made a navmesh and a bunch of other little tweaks to things in the game.

--- Adventure_Game/Assets/SceneStorage/OverallScripts/TriggerParticleEffect.cs
+++ Adventure_Game/Assets/SceneStorage/OverallScripts/TriggerParticleEffect.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
-    private ParticleSystem particleSystem;
-    public int particleAmount = 10;
+    public new ParticleSystem particleSystem;
+    public int firstParticleAmount = 10;
+    public int secondParticleAmount = 20;
+    public int thirdParticleAmount = 30;
+    public float minTime = 0.1f;
+    public float maxTime = 1f;
     public float minSpeed = 0.1f;
     public float maxSpeed = 1f;
 
@@ -19,10 +23,22 @@ public class TriggerParticleEffect : MonoBehaviour
     {
         if (other.gameObject.GetComponent<CharacterController>())
         {
+            StartCoroutine(EmitParticlesCoroutine());
+            
             var main = particleSystem.main;
             main.startSpeed = Random.Range(minSpeed, maxSpeed);
-            
-            particleSystem.Emit(particleAmount);
         }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(firstParticleAmount);
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+
+        particleSystem.Emit(secondParticleAmount);
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        
+        particleSystem.Emit(thirdParticleAmount);
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
     }
 }
